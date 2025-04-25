3a902d88b9c2b2af63fcc7b207ebc9aab478d48e
pankaj kumar <pankajkumar12861@gmail.com>
2025-04-25 09:56:25 +0530

    Link tag wrapping

--- components/profileCard/index.js
+++ components/profileCard/index.js
@@ -3,23 +3,26 @@ import buyerImg from "@/public/images/buyer.png";
 import sellerImg from "@/public/images/seller.png";
 import Image from "next/image";
 import { BUYER, SELLER } from "@/utils/constants";
+import Link from "next/link";
 
 const ProfileCard = ({ title }) => {
   return (
-    <div className="relative w-full max-w-xs p-6 rounded-2xl bg-white/10 backdrop-blur-md shadow-xl border border-white/20 text-white transition-transform hover:scale-105 duration-300 cursor-pointer">
-      {/* Optional icon or emoji */}
-      <div className="text-5xl mb-4">
-        {title === BUYER ? (
-          <Image height={350} width={350} alt={BUYER} src={buyerImg} />
-        ) : (
-          <Image height={350} width={350} alt={SELLER} src={sellerImg} />
-        )}
+    <Link href={title === BUYER ? "/listProducts" : "products"}>
+      <div className="relative w-full max-w-xs p-6 rounded-2xl bg-white/10 backdrop-blur-md shadow-xl border border-white/20 text-white transition-transform hover:scale-105 duration-300 cursor-pointer">
+        {/* Optional icon or emoji */}
+        <div className="text-5xl mb-4">
+          {title === BUYER ? (
+            <Image height={350} width={350} alt={BUYER} src={buyerImg} />
+          ) : (
+            <Image height={350} width={350} alt={SELLER} src={sellerImg} />
+          )}
+        </div>
+        <div className=" flex text-xl text-black justify-center font-semibold">
+          {" "}
+          {title}
+        </div>
       </div>
-      <div className=" flex text-xl text-black justify-center font-semibold">
-        {" "}
-        {title}
-      </div>
-    </div>
+    </Link>
   );
 };
 
