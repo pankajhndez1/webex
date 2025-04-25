6edcec80e1bcd347efd61b51001b8528eaf93bf7
pankaj kumar <pankajkumar12861@gmail.com>
2025-04-25 19:47:19 +0530

    some css fixes

--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -61,9 +61,9 @@ export default function ProductDetail() {
 
 
   return (
-    <div className="max-w-6xl mx-auto p-4 bg-white">
+    <div className="max-w-6xl mx-auto p-4 mt-5 bg-white">
       
-      <div className="mb-6 pt-20">
+      <div className="mb-6 pt-2">
         <Link
           href="/products"
           className="flex items-center text-lg font-medium"
@@ -73,7 +73,7 @@ export default function ProductDetail() {
         </Link>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 ">
         
         <div className="flex flex-col">
           <div className="mb-4 border rounded-lg overflow-hidden">
