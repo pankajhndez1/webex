d6d4da7b3f7f715076e7eee6a1e93566a30d14a9
pankaj kumar <pankajkumar12861@gmail.com>
2025-05-03 16:23:07 +0530

    minor changes

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,9 @@
 
 import NextAuth from 'next-auth'
-import AppleProvider from 'next-auth/providers/apple'
-import FacebookProvider from 'next-auth/providers/facebook'
-import GoogleProvider from 'next-auth/providers/google'
-import EmailProvider from 'next-auth/providers/email'
+// import AppleProvider from 'next-auth/providers/apple'
+// import FacebookProvider from 'next-auth/providers/facebook'
+// import GoogleProvider from 'next-auth/providers/google'
+// import EmailProvider from 'next-auth/providers/email'
 import GithubProvider from "next-auth/providers/github"
 
 const handler= NextAuth({
